e951cb2e8f9974bf83c2ee17b1b9db8ed83a740f
bwaromale <bwaromalelucky@gmail.com>
2023-03-03 13:58:52 +0100

    Main menu with functional Signup and Login

---
+++ FraudulentBank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FraudulentBank
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "accounts.txt");
+
+            Console.WriteLine("Welcome to the Fraudulent Bank application!");
+
+            while (true)
+            {
+                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("1. Signup");
+                Console.WriteLine("2. Login");
+                Console.WriteLine("3. Exit");
+
+                Console.WriteLine("Choice: ");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        UserAccount.Signup(fileName);
+                        break;
+                    case "2":
+                        UserAccount.Login(fileName);
+                        break;
+                    case "3":
+                        Console.WriteLine("Have a nice day");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option Selection");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ FraudulentBank/UserAccount.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+public class UserAccount
+{
+    public static void Signup(string fileName)
+    {
+        Console.WriteLine("Please enter your name:");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Please enter your email:");
+        var email = Console.ReadLine();
+
+        Console.WriteLine("Please enter your password:");
+        var password = Console.ReadLine();
+
+        // Generate a unique 10-digit account number starting with 2
+        Random rand = new Random();
+        string accountNumber = "2" + rand.Next(100000000, 999999999).ToString();
+
+        using (StreamWriter sw = File.AppendText(fileName))
+        {
+            sw.WriteLine($"{accountNumber},{name},{email},{password},0");
+        }
+
+        Console.WriteLine("Signup successful!");
+        Console.WriteLine("Your account number is " + accountNumber);
+    }
+    public static string Login(string fileName)
+    {
+        Console.WriteLine("Please enter your account number:");
+        var accountNumber = Console.ReadLine();
+
+        Console.WriteLine("Please enter your password:");
+        Console.Write("Enter password: ");
+        string password = "";
+        while (true)
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                break;
+            }
+            else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+            {
+                password = password.Substring(0, password.Length - 1);
+                Console.Write("\b \b");
+            }
+            else if (!char.IsControl(key.KeyChar))
+            {
+                password += key.KeyChar;
+                Console.Write("*");
+            }
+        }
+
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+            string[] values = line.Split(',');
+
+            if (values[0] == accountNumber && values[3] == password)
+            {
+                Console.WriteLine("Login successful!");
+                return accountNumber;
+            }
+        }
+
+        Console.WriteLine("Invalid account number or password. Please try again.");
+        return null;
+    }
+}
